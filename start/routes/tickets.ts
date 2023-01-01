a27f9c29ab77fec84db29b761464c6460c310883
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-01-01 23:24:25 +0100

    feat: created history route

--- start/routes/tickets.ts
+++ start/routes/tickets.ts
@@ -2,6 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('', 'TicketsController.getList')
+  Route.get('history', 'TicketsController.getHistory')
   Route.post(':id/activate', 'TicketsController.activate')
   Route.get(':id', 'TicketsController.get')
 }).prefix('tickets')
